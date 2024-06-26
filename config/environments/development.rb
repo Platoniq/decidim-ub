3044ff6864f270b3ba53148ff4e6c0dcb7a00a90
Harrison Hassig <harrisonhassig@gmail.com>
2024-06-26 07:56:52 -0700

    Linting and specs (#3)
    * Added .github/workflow file for CI
    
    * Lint ERB files with erblint
    
    * Lint SCSS files with stylelint
    
    * Add i18n spec
    
    * Lint JS files with eslint
    
    * Lint MarkDown files with mdl
    
    * Default local to :ca
    
    * Rubocop automatic fixes
    
    * db:migrate to create db/schema.rb
    
    * Revert "Default local to :ca"
    
    This reverts commit 00532b0bd9fc43d9713977479e1b2d92a2c72b02.
    
    * Run Rubocop
    
    * Rubocop corrections
    
    * Restored db/migrate files to before rubocop fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
@@ -16,13 +18,13 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp/caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -58,10 +60,8 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  
 
   # Suppress logger output for asset requests.
-  
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true

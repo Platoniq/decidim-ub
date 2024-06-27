29450b9c19c900c0688f2b16dd5afeab9c5ea5c7
Harrison Hassig <harrisonhassig@gmail.com>
2024-06-27 07:05:01 -0700

    correct platoniq footer spec

--- spec/system/platoniq_footer_spec.rb
+++ spec/system/platoniq_footer_spec.rb
@@ -2,10 +2,11 @@
 
 require "rails_helper"
 
-describe "Has EU footer", perform_enqueued: true do
+describe "Has Platoniq footer", perform_enqueued: true do
   include_context "when visiting organization homepage"
 
-  it "renders the eu flag in the footer" do
-    expect(page).to have_xpath("//img[@alt='European Union flag: Funded by the European Union']")
+  it "renders the platoniq logo in the footer" do
+    expect(page).to have_content("Made with ♥ by")
+    expect(page).to have_xpath("//img[@alt='Platoniq Foundation - Creativity and Democracy']")
   end
 end

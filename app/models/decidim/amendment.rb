afb94efce33199ae7288b325081d8bf567450e94
Harrison Hassig <harrisonhassig@gmail.com>
2025-02-04 17:28:01 +0100

    lint and imagemagick for test.yml

--- app/models/decidim/amendment.rb
+++ app/models/decidim/amendment.rb
@@ -1,37 +1,37 @@
 # frozen_string_literal: true
 
 module Decidim
-    class Amendment < ApplicationRecord
-      STATES = { draft: 0, evaluating: 10, accepted: 20, rejected: 30, withdrawn: -1 }.freeze
-  
-      belongs_to :amendable, foreign_key: "decidim_amendable_id", foreign_type: "decidim_amendable_type", polymorphic: true
-      belongs_to :amender, foreign_key: "decidim_user_id", class_name: "Decidim::User"
-      belongs_to :emendation, foreign_key: "decidim_emendation_id", foreign_type: "decidim_emendation_type", polymorphic: true
-  
-      validates :state, presence: true, inclusion: { in: STATES.keys.map(&:to_s) }
-  
-      enum state: STATES, _default: "draft"
-      
-      after_save :update_proposal_amendments_count
-      after_destroy :update_proposal_amendments_count
-  
-      # Reports the mapped resource type for authorization transfers.
-      #
-      # @return [String] The resource type as string (i.e. its class name).
-      def mapped_resource_type
-        decidim_amendable_type
-      end
-  
-      def promoted?
-        return false unless rejected?
-  
-        emendation.linked_promoted_resource.present?
-      end
+  class Amendment < ApplicationRecord
+    STATES = { draft: 0, evaluating: 10, accepted: 20, rejected: 30, withdrawn: -1 }.freeze
 
-      private
+    belongs_to :amendable, foreign_key: "decidim_amendable_id", foreign_type: "decidim_amendable_type", polymorphic: true
+    belongs_to :amender, foreign_key: "decidim_user_id", class_name: "Decidim::User"
+    belongs_to :emendation, foreign_key: "decidim_emendation_id", foreign_type: "decidim_emendation_type", polymorphic: true
 
-      def update_proposal_amendments_count
-        Decidim::Proposals::Proposal.find(decidim_amendable_id).update_amendments_count
-      end
+    validates :state, presence: true, inclusion: { in: STATES.keys.map(&:to_s) }
+
+    enum state: STATES, _default: "draft"
+
+    after_destroy :update_proposal_amendments_count
+    after_save :update_proposal_amendments_count
+
+    # Reports the mapped resource type for authorization transfers.
+    #
+    # @return [String] The resource type as string (i.e. its class name).
+    def mapped_resource_type
+      decidim_amendable_type
+    end
+
+    def promoted?
+      return false unless rejected?
+
+      emendation.linked_promoted_resource.present?
+    end
+
+    private
+
+    def update_proposal_amendments_count
+      Decidim::Proposals::Proposal.find(decidim_amendable_id).update_amendments_count
     end
   end
+end
--- app/models/decidim/proposals/proposal_amendment.rb
+++ app/models/decidim/proposals/proposal_amendment.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
 module Decidim
-    module Proposals
-      # A proposal can include many amendments
-      class ProposalAmendment < ApplicationRecord
-        belongs_to :proposal, foreign_key: "decidim_proposal_id", class_name: "Decidim::Proposals::Proposal"
-        has_many :emendations, through: :proposals, source: :emendations
-  
-        validates :proposal, uniqueness: { scope: :author }
-        validate :author_and_proposal_same_organization
-        validate :proposal_not_rejected
+  module Proposals
+    # A proposal can include many amendments
+    class ProposalAmendment < ApplicationRecord
+      belongs_to :proposal, foreign_key: "decidim_proposal_id", class_name: "Decidim::Proposals::Proposal"
+      has_many :emendations, through: :proposals, source: :emendations
 
-        def self.temporary
-          where(temporary: true)
-        end
-  
-        # Final amendments are amendments that will be taken into account, that is, they are
-        # not temporary.
-        def self.final
-          where(temporary: false)
-        end
-  
-        private
-  
-        def proposal_not_rejected
-          return unless proposal
-  
-          errors.add(:proposal, :invalid) if proposal.rejected?
-        end
+      validates :proposal, uniqueness: { scope: :author }
+      validate :author_and_proposal_same_organization
+      validate :proposal_not_rejected
+
+      def self.temporary
+        where(temporary: true)
+      end
+
+      # Final amendments are amendments that will be taken into account, that is, they are
+      # not temporary.
+      def self.final
+        where(temporary: false)
+      end
+
+      private
+
+      def proposal_not_rejected
+        return unless proposal
+
+        errors.add(:proposal, :invalid) if proposal.rejected?
       end
     end
   end
+end

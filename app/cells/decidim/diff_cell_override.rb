ddcd13c99e8aedcfe6aebaf20c2bf8fb56bbeea8
Harrison Hassig <harrisonhassig@gmail.com>
2025-07-09 17:04:12 +0200

    Slight refactoring

--- app/cells/decidim/diff_cell_override.rb
+++ app/cells/decidim/diff_cell_override.rb
@@ -1,25 +1,21 @@
 # frozen_string_literal: true
 
-# diff_cell_override.rb
 module Decidim
   module DiffCellOverride
     extend ActiveSupport::Concern
 
     included do
-      # Override the unified_mode? method to change default behavior
       def unified_mode?
         @unified_mode ||= begin
           mode = params["diff-mode"]
-          mode.nil? ? false : mode != "split" # Default to false (split mode) when nil
+          mode.nil? ? false : mode != "split" #
         end
       end
 
-      # Add your custom method
       def current_diff_mode
-        params["diff-mode"] || "split" # Default to split instead of unified
+        params["diff-mode"] || "split"
       end
 
-      # Override mode_options if you want to change the default selection
       def mode_options
         dropdown_options_for_select(%w(unified split), current_diff_mode)
       end

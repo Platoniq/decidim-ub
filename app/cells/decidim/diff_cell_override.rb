667a9a54351404fbeb1394b6df2d22ea8c282eae
Harrison Hassig <harrisonhassig@gmail.com>
2025-07-10 13:06:32 +0200

    Restore diff_cell_override.rb

---
+++ app/cells/decidim/diff_cell_override.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Decidim
+  module DiffCellOverride
+    extend ActiveSupport::Concern
+
+    included do
+      def unified_mode?
+        @unified_mode ||= begin
+          mode = params["diff-mode"]
+          mode.nil? ? false : mode != "split"
+        end
+      end
+
+      def current_diff_mode
+        params["diff-mode"] || "split"
+      end
+
+      def mode_options
+        dropdown_options_for_select(%w(unified split), current_diff_mode)
+      end
+    end
+  end
+end
